ac8095ba53ea0bb5936e286acc7209ab7946e720
Jordisunda1 <162414577+Jordisunda1@users.noreply.github.com>
2024-10-29 10:50:15 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const tonConnectConfig = {
+    manifestUrl: "https://yourdomain.com/tonconnect-manifest.json", // URL manifest
+};
+const contractAddress = "0:123456789abcdef..."; // Ganti dengan alamat kontrak TON
